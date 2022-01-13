76a685ab64dd162cf863020dd1f5a7b0efe2d049
Ratnesh Maurya <ratneshmaurya2001@gmail.com>
2022-01-13 17:49:29 +0530

    updated file

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -11,8 +11,8 @@ const links=[
 const Links = () => {
     return (
         <div className='flex sm:justify-around justify-between items-center mt-4 mb-3'>
-            {links.map(({url,text},i)=>(
-                <NavLink key={i} exact="true" to={url} activeclassname='text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2'  className='m-2 mb-0'>
+            {links.map(({url,text},index)=>(
+                <NavLink key={index} exact="true" to={url} activeclassname='text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2'  className='m-2 mb-0'>
                     {text}
                 </NavLink>
             ))}
